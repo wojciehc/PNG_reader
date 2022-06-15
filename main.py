0571b0d653dba4bdb1a212c63cf2ad6235c1279b
gmtrk <gmtrkk@gmail.com>
2022-06-15 16:24:10 +0200

    dodano funkcje do szyfrowania RSA

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from binascii import crc32
 
 import cv2
 import numpy as np
+import random
+import math
 import matplotlib.pyplot as plt
 from skimage.io import imread, imshow, imsave
 from skimage.color import rgb2hsv, rgb2gray, rgb2yuv, rgba2rgb
@@ -150,6 +152,103 @@ class PNG:
                     file.write(crc32(chunk[0] + chunk[1]).to_bytes(4, 'big'))
 
 
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+'''
+Euclid's extended algorithm for finding the multiplicative inverse of two numbers
+'''
+
+
+def multiplicative_inverse(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+
+    while e > 0:
+        temp1 = temp_phi / e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
+
+        x = x2 - temp1 * x1
+        y = d - temp1 * y1
+
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+
+    if temp_phi == 1:
+        return d + phi
+
+
+'''
+Tests to see if a number is prime.
+'''
+
+
+def is_prime(num):
+    if num == 2:
+        return True
+    if num < 2 or num % 2 == 0:
+        return False
+    for n in range(3, int(num ** 0.5) + 2, 2):
+        if num % n == 0:
+            return False
+    return True
+
+
+def generate_keypair(p, q):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+    # n = pq
+    n = p * q
+
+    # Phi is the totient of n
+    phi = (p - 1) * (q - 1)
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
+
+
+def encrypt(pk, plaintext):
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on the character using a^b mod m
+    cipher = [pow(ord(char), key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return ''.join(plain)
+
 
 if __name__ == '__main__':
     print('Chunki w pliku przed animizacja:')
@@ -170,13 +269,24 @@ if __name__ == '__main__':
     dark_image_fft = np.fft.fftshift(np.fft.fft2(dark_image))
     inverse_fft = np.fft.ifft2(np.fft.ifftshift(dark_image_fft))
     ift_real = inverse_fft.real
+    imsave("angle.png", np.angle(dark_image_fft))
     imsave("fft.png", np.log(abs(dark_image_fft)))
     imsave("ifft.png", ift_real)
     from PIL import Image
-    im = Image.open('out.png')
-    im.show()
+    #im = Image.open('out.png')
+    #im.show()
 
-    Image.open("grey.png").show()
-    Image.open('fft.png').show()
-    Image.open('ifft.png').show()
 
+    #Image.open("grey.png").show()
+    #Image.open('fft.png').show()
+    #Image.open('ifft.png').show()
+    #Image.open('angle.png').show()
+
+    p=23
+    q=17
+    public, private = generate_keypair(p, q)
+    message = "ttttttt"
+    encrypted_msg = encrypt(private, message)
+    print (''.join(map(lambda x: str(x), encrypted_msg)))
+    print ('Your message is:')
+    print (decrypt(public, encrypted_msg))
